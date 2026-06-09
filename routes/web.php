a5fc31d1844dd2515e171354d9e5df512f7960cf
unknown <iptwahyueka@gmail.com>
2026-06-09 21:27:18 +0800

    add: create OTP

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
+Route::view('/otp-wa', 'Verifikasi-OTP-WA')->name('Verifikasi-OTP-WA');
+Route::view('/klaim-reward', 'klaim-reward')->name('klaim-reward');
