66c1641c60cddf6a484829263acff25b015e16c9
ayutitania <ayutitania135@gmail.com>
2026-06-10 00:18:18 +0800

    add: tata dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name('home');
 
 Route::view('/login', 'login')->name('login');
+Route::view('/Dashboard-Saya', 'Dashboard-Saya')->name('Dashboard-Saya');
 
 // nah kayak gitu, untuk parameter pertama itu urlnya contoh /register atau /login (jangan isi spasi)
 // parameter kedua itu nama filenya (karena ak buatnya login.blade.php jadi isiinya login)
