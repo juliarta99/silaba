ad8f43e4c51118ea559d068875090813f558e36e
juliarta99 <adij4255@gmail.com>
2026-06-10 14:07:33 +0800

    revert: to main

--- resources/views/login.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app') 
-
-@section('content')
-    <div class="">
-        <h1>INi login</h1> 
-        {{-- cek lagi setelah ubah --}}
-        {{-- sudah lanjut ngoding --}}
-    </div>
-@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 
 @section('content')
     <div class="">
-        <h1>Haloo</h1>
+        <h1>ini welcome</h1> 
+        {{-- cek lagi setelah ubah --}}
+        {{-- sudah lanjut ngoding --}}
     </div>
 @endsection
